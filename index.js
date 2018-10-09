ebc89b46ecfbfc3f40f55f471229e1b9b910828d
Christian Broberg <cb@webhouse.dk>
2018-10-10 01:38:57 +0200

    Version bump

--- index.js
+++ index.js
@@ -10,8 +10,10 @@
 // 	return "MODULE 1"
 // }
 
-module.exports = function tiny(string) {
-	if (typeof string !== "string") throw new TypeError("Tiny wants a string!")
-	return string.replace(/\s/g, "")
-}
+// module.exports = function tiny(string) {
+// 	if (typeof string !== "string") throw new TypeError("Tiny wants a string!")
+// 	return string.replace(/\s/g, "")
+// }
+
+exports.tiny = require('./lib/tiny')
 
