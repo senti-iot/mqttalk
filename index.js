9694f203c5aa67b04587dff64b8020e977e6a9a3
Christian Broberg <cb@webhouse.dk>
2018-10-10 01:22:00 +0200

    Up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-exports.module1 = require('./lib/module1')
+exports.module1 = require('./lib/module1').module1
 exports.module2 = require('./lib/module2')
 exports.module3 = require('./lib/module3')
+exports.tiny = require('./lib/module1').tiny
 
 // module.exports = { module1, module2, module3 }
 
