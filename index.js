f48aca995acaca2b2f0564a3489cdaa476c4b9f4
Christian Broberg <cb@webhouse.dk>
2018-10-10 01:03:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.module1 = require('./lib/module1')
+exports.module2 = require('./lib/module2')
+exports.module3 = require('./lib/module3')
+
+// module.exports = { module1, module2, module3 }
+
