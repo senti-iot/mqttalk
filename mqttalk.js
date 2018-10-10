a7a3a971d5292356e049206c8a012b74c419b60d
Christian Broberg <cb@webhouse.dk>
2018-10-10 02:18:48 +0200

    mqttalk.js is born

---
+++ mqttalk.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+'use strict'
+
+/*
+ * Copyright (c) 2018-2018 mqttalk.js contributors.
+ * Copyright (c) 2018-2018 Christian Broberg.
+ *
+ * See LICENSE for more information
+ */
+
+
+ // Import modules
+var module1 = require('./lib/module1')
+var module2 = require('./lib/module2')
+var module3 = require('./lib/module3')
+var tiny = require('./lib/tiny')
+
+// Expose mqttalk modules
+module.exports.module1 = module1
+module.exports.module2 = module2
+module.exports.module2 = module3
+module.exports.tiny = tiny
+
